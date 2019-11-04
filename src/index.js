4838165d13fdb95c163a10aeddf606ab87bb188d
TarasProtsyshyn <taras1procishin@gmail.com>
2019-11-04 17:49:14 +0200

    add proto observer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,62 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const accessedProperties = [];
+const derivationGraph = {};
+
+function observable() {}
+
+function autorun(cb) {}
+
+// const store = Observable({
+//   count: 0,
+//   name: 'Unnamed',
+
+//   incremrnt() {
+//     this.count += 1;
+//   },
+
+//   setName() {
+//     this.name = "Test";
+//   },
+
+//   get countPowToSquere() {
+//     return this._count * this._count;
+//   }
+// })
+
+// class Observable {
+//   constructor(val) {
+//     this._value = val;
+
+//     this._listeners = [];
+//   }
+
+//   subscribe(cb) {
+//     this._listeners.push(() => cb(this._value));
+//   }
+
+//   next(val) {
+//     this._value = val;
+
+//     this._listeners.forEach(cb => {
+//       cb();
+//     })
+//   }
+// }
+
+// const custumObsorvebal = new Observable(1);
+
+// custumObsorvebal.subscribe(currVal => {
+//   console.log(currVal)
+// })
+
+// custumObsorvebal.subscribe(currVal => {
+//   console.log(currVal + 1)
+// })
+
+// custumObsorvebal.next(2)
+
 function App() {
   return (
     <div className="App">
